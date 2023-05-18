b4c072a32608f4bc3c40055549a8bc6d69dfe65e
paneladev <paneladev@gmail.com>
2023-05-18 16:22:17 -0300

    aplicacao frontend react

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamJuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'NLW Spacetime Next App',
+  description:
+    'Uma cápsula do tempo construída com React, Next.js, Node.js, Prisma',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamJuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
