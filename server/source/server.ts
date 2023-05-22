1c1de08a4f99b14b5352d18d16d2b1467fa76654
paneladev <paneladev@gmail.com>
2023-05-22 14:32:51 -0300

    ajuste para visualização da imagem pelo backend

--- server/source/server.ts
+++ server/source/server.ts
@@ -5,8 +5,18 @@ import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import { authRoutes } from './routes/auth'
+import multipartFile from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(multipartFile)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // todas urls de front podem acessar nosso backend
@@ -19,6 +29,7 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
