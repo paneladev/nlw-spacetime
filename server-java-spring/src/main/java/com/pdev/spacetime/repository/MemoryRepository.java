6b8f80cfd6fa411de43cf0eadcfa693c0f4e7463
paneladev <paneladev@gmail.com>
2023-05-24 16:30:08 -0300

    feat: listando memorias publicas ou do usuario logado

--- server-java-spring/src/main/java/com/pdev/spacetime/repository/MemoryRepository.java
+++ server-java-spring/src/main/java/com/pdev/spacetime/repository/MemoryRepository.java
@@ -1,9 +1,14 @@
 package com.pdev.spacetime.repository;
 
 import com.pdev.spacetime.entity.Memory;
+import com.pdev.spacetime.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemoryRepository extends JpaRepository<Memory, Long> {
+
+    List<Memory> findByUserOrIsPublicTrue(User user);
 }
