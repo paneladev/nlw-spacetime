29aa7ca461cac6d8dd76a3d16f74d07e26d366e8
paneladev <paneladev@gmail.com>
2023-05-24 16:41:01 -0300

    fix: mostrar imagem a partir da url

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -45,8 +45,8 @@ export default async function Home() {
             <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
               {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
             </time>
-            <img
-              src={`data:image/jpeg;base64,${memory.coverImage}`}
+            <Image
+              src={memory.coverUrl}
               alt=""
               width={592}
               height={280}
